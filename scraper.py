d8c2f8f139f9af621fb26e9de36b640da0564660
Emily Tran <emilytt4@uci.edu>
2025-11-04 00:09:27 -0800

    just added starter code and filled ids

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin, urlsplit, urldefrag
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,19 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+    resp_links = []
+    if resp.status == 200 & resp.raw_response != None:
+        content = resp.raw_response.content
+        if not content or len(content) == 0:
+            return resp_links
+
+        soup = BeautifulSoup(content, "html.parser")
+        for tag in soup.find_all("a", href=True):
+            link = urljoin(url, tag["href"])
+            defragged, _ = urldefrag(link)  # remove fragments?
+            resp_links.append(defragged)
+
+    return resp_links
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
@@ -25,6 +38,16 @@ def is_valid(url):
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
             return False
+        
+        # Detect and avoid infinite traps
+        path_depth = parsed.path.split('/')
+        if len(path_depth) > 8:
+            return False
+
+        # Detect and avoid sets of similar pages with no information
+        # Detect and avoid dead URLs that return a 200 status but no data
+        # Detect and avoid crawling very large files, especially if they have low information value
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
